a9c98f7f6c03fc3f4dfbacaab1465d2d08338951
Toni Karppi <karppitoni@gmail.com>
2019-03-13 18:58:01 +0100

    Format with black

--- resopurge.py
+++ resopurge.py
@@ -1,3 +1,8 @@
+"""
+This script is used for removing image files that have a lower width/height than
+the specified values.
+"""
+
 import os
 from getpass import getuser
 from PIL import Image
@@ -11,8 +16,7 @@ MIN_HEIGHT = 1080
 
 
 def get_files_recursively(root_path):
-    files_in_paths = [(root, files)
-                      for root, dirs, files in os.walk(root_path)]
+    files_in_paths = [(root, files) for root, dirs, files in os.walk(root_path)]
     return [
         f"{path}/{file}"
         for path, files in files_in_paths
@@ -33,12 +37,15 @@ def delete_files(files):
 
 def main():
     image_files = get_files_recursively(IMAGES_PATH)
-    small_images = [image
-                    for image in image_files
-                    if image_smaller_than(image, MIN_WIDTH, MIN_HEIGHT)]
+    small_images = [
+        image
+        for image in image_files
+        if image_smaller_than(image, MIN_WIDTH, MIN_HEIGHT)
+    ]
 
     answer = input(
-        f"You are about to remove {len(small_images)} files. Continue? y/[n]\n")
+        f"You are about to remove {len(small_images)} files. Continue? y/[n]\n"
+    )
 
     if answer.lower() != "y":
         print("Aborting.")
